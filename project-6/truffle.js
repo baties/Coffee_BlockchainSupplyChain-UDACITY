f7d60795d8e09d1796ea453f1b125135bcd689b1
Batis <abba713@gmail.com>
2021-08-31 18:47:03 +0430

    Complete Test Contract

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -2,15 +2,15 @@ const HDWalletProvider = require('truffle-hdwallet-provider');
 const infuraKey = "1d3512198c8345a1b1816981c999c328";
 //
 // const fs = require('fs');
-const mnemonic = "weasel core blue sudden ahead chronic scout style subject payment clock affair";
-// const mnemonic = "spirit supply whale amount human item harsh scare congress discover talent hamster" ;
+// const mnemonic = "weasel core blue sudden ahead chronic scout style subject payment clock affair";
+const mnemonic = "spirit supply whale amount human item harsh scare congress discover talent hamster" ;
 
 module.exports = {
 
   networks: {
     host: "127.0.0.1",     // Localhost (default: none)
     development: {
-      port: 9545,            // Standard Ethereum port (default: none)
+      port: 7545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
      },
     // Useful for deploying to a public network.
